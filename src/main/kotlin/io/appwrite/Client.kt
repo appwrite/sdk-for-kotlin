da6497f7aa38551496b0d3141721d4d337ae7c13
Christy Jacob <christyjacob4@gmail.com>
2023-09-07 21:35:36 +0000

    chore: updates for appwrite 1.4.2

--- src/main/kotlin/io/appwrite/Client.kt
+++ src/main/kotlin/io/appwrite/Client.kt
@@ -62,11 +62,11 @@ class Client @JvmOverloads constructor(
     init {
         headers = mutableMapOf(
             "content-type" to "application/json",
-            "user-agent" to "AppwriteKotlinSDK/3.0.1 ${System.getProperty("http.agent")}",
+            "user-agent" to "AppwriteKotlinSDK/3.0.2 ${System.getProperty("http.agent")}",
             "x-sdk-name" to "Kotlin",
             "x-sdk-platform" to "server",
             "x-sdk-language" to "kotlin",
-            "x-sdk-version" to "3.0.1",            "x-appwrite-response-format" to "1.4.0"
+            "x-sdk-version" to "3.0.2",            "x-appwrite-response-format" to "1.4.0"
         )
         config = mutableMapOf()
 
