0a604d3fb668e3a59b8126b98daba89ecc2171c6
root <chiragaggarwal5k@gmail.com>
2026-02-02 12:32:44 +0000

    update changelog etc

---
+++ src/main/kotlin/io/appwrite/models/AttributeMediumtext.kt
@@ -0,0 +1,95 @@
+package io.appwrite.models
+
+import com.google.gson.annotations.SerializedName
+import io.appwrite.extensions.jsonCast
+import io.appwrite.enums.AttributeStatus
+
+/**
+ * AttributeMediumtext
+ */
+data class AttributeMediumtext(
+    /**
+     * Attribute Key.
+     */
+    @SerializedName("key")
+    val key: String,
+
+    /**
+     * Attribute type.
+     */
+    @SerializedName("type")
+    val type: String,
+
+    /**
+     * Attribute status. Possible values: `available`, `processing`, `deleting`, `stuck`, or `failed`
+     */
+    @SerializedName("status")
+    val status: AttributeStatus,
+
+    /**
+     * Error message. Displays error generated on failure of creating or deleting an attribute.
+     */
+    @SerializedName("error")
+    val error: String,
+
+    /**
+     * Is attribute required?
+     */
+    @SerializedName("required")
+    val required: Boolean,
+
+    /**
+     * Is attribute an array?
+     */
+    @SerializedName("array")
+    var array: Boolean?,
+
+    /**
+     * Attribute creation date in ISO 8601 format.
+     */
+    @SerializedName("\$createdAt")
+    val createdAt: String,
+
+    /**
+     * Attribute update date in ISO 8601 format.
+     */
+    @SerializedName("\$updatedAt")
+    val updatedAt: String,
+
+    /**
+     * Default value for attribute when not provided. Cannot be set when attribute is required.
+     */
+    @SerializedName("default")
+    var default: String?,
+
+) {
+    fun toMap(): Map<String, Any> = mapOf(
+        "key" to key as Any,
+        "type" to type as Any,
+        "status" to status.value as Any,
+        "error" to error as Any,
+        "required" to required as Any,
+        "array" to array as Any,
+        "\$createdAt" to createdAt as Any,
+        "\$updatedAt" to updatedAt as Any,
+        "default" to default as Any,
+    )
+
+    companion object {
+
+        @Suppress("UNCHECKED_CAST")
+        fun from(
+            map: Map<String, Any>,
+        ) = AttributeMediumtext(
+            key = map["key"] as String,
+            type = map["type"] as String,
+            status = AttributeStatus.values().find { it.value == map["status"] as String }!!,
+            error = map["error"] as String,
+            required = map["required"] as Boolean,
+            array = map["array"] as? Boolean,
+            createdAt = map["\$createdAt"] as String,
+            updatedAt = map["\$updatedAt"] as String,
+            default = map["default"] as? String,
+        )
+    }
+}
