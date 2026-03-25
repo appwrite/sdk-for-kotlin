3f626360f11d5801b674ebc3e7c01313aa3ac9d6
root <chiragaggarwal5k@gmail.com>
2026-03-25 06:44:55 +0000

    chore: update Kotlin SDK to 15.0.0

--- src/main/kotlin/io/appwrite/enums/VCSReferenceType.kt
+++ src/main/kotlin/io/appwrite/enums/VCSReferenceType.kt
@@ -6,9 +6,7 @@ enum class VCSReferenceType(val value: String) {
     @SerializedName("branch")
     BRANCH("branch"),
     @SerializedName("commit")
-    COMMIT("commit"),
-    @SerializedName("tag")
-    TAG("tag");
+    COMMIT("commit");
 
     override fun toString() = value
 }
